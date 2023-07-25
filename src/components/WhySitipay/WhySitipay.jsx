9220a70d3acbcebf34d7610e16c94007a8968e7a
mzakrzewska1 <120480139+mzmoonsun@users.noreply.github.com>
2023-07-25 22:44:27 +0100

    add whySitipay component

---
+++ src/components/WhySitipay/WhySitipay.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './whysitipay.css'
+import schoolIcon from '../../img/school.svg'
+import pigIcon from '../../img/savings.svg'
+import dollarIcon from '../../img/currency_exchange.svg'
+
+const WhySitipay = () => {
+  return (
+      <div className='main-why'>
+
+          <div className='title-div title-margin'> 
+              <p>Why Sitipay</p>
+            <h1>Preparing You for <br />
+                <span>Your Future, Today</span></h1>
+          </div>
+          <div className='cards-div'>
+              <div className='why-card'>
+                  <h3>Financial Education</h3>
+                  <img src={schoolIcon} alt="icon" />
+                  <p>Stay informed and make smart financial decisions.</p>
+              </div>
+
+              <div className='why-card'>
+                  <h3>Financial Education</h3>
+                  <img src={pigIcon} alt="icon" />
+                  <p>Stay informed and make smart financial decisions.</p>
+              </div>
+
+              <div className='why-card'>
+                  <h3>Financial Education</h3>
+                  <img src={dollarIcon} alt="icon" />
+                  <p>Stay informed and make smart financial decisions.</p>
+              </div>
+
+          </div>
+    </div>
+  )
+}
+
+export default WhySitipay
