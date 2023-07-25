4de2ab6ea4d87c6b955f0c5ffe64b6e0115b4238
mzakrzewska1 <120480139+mzmoonsun@users.noreply.github.com>
2023-07-25 11:22:17 +0100

    add first draft of mobile navbar and Opening compnent

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import './App.css'
+
+import './App.css';
+import NavBar from './components/Navbar/NavBar.jsx';
+import Opening from './components/Opening/Opening';
+// import Features from './components/Features/Features';
+// import EmpoweringStudents from './components/EmpoweringStudents.jsx/EmpoweringStudents';
+// import WhySitipay from './components/WhySitipay/WhySitipay';
+// import Testimonials from './components/Testimonials/Testimonials'
+// import Timeline from './components/Timeline/Timeline';
+// import AboutUs from './components/AboutUs/AboutUs';
+// import Footer from './components/Footer/Footer';
+
 
 function App() {
-
   return (
-    <div>
+    <div className="App">
+      <NavBar />
+      <Opening />
+      {/* 
+      <Features />
+      <EmpoweringStudents />
+      <WhySitipay />
+      <Testimonials />
+      <Timeline />
+      <AboutUs />
+      <Footer /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
